11ce797426b61ea81d9f50c4869de1c82a14e9c1
Kevin <Lin_Kevin_1995@hotmail.com>
2017-10-01 16:45:36 -0400

    Simplified code.

--- A1/Q1/Reverse.java
+++ A1/Q1/Reverse.java
@@ -11,44 +11,29 @@ package Q1;
  */
 public class Reverse {
     public static void main(String[] args) {
-        int array[] = {1,2,3,4,5,6,7,8,9,10,11,12};
-        System.out.print("Input array: ");
-        for (int i = 0; i < array.length; i++) {
-            System.out.print(array[i] + " ");
-        }
-        int x, y, z;
-        int a, b, c, d;
+        int a[] = {1,2,3,4,5,6,7,8,9,10,11};
+        reverse(a);
+    }
+    
+    public static void reverse(int input[]) {
+        int array[] = input;
+        int a = 0, b = ((array.length)/2) - 1, c, d = array.length -1, temp;
         if (array.length >= 1) {
-            a = 0;
-            b = ((array.length)/2);
             if (array.length % 2 == 0) {
                 c = ((array.length)/2);
             } else {
                 c = ((array.length)/2) + 1;
             }
-            d = array.length - 1;
-            while (a < b - 1) {
-                    x = array[a];
-                    y = array[a+1];
-                    z = x;
-                    x = y;
-                    y = z;
-                    array[a] = x;
-                    array[a+1] = z;
+            while (a < b) {
+                    temp = array[a];
+                    array[a] = array[a+1];
+                    array[a+1] = temp;
                     a += 2;
             }
             while (c < d) {
-                x = array[c];
-                y = array[c+1];
-                z = x + y;
-                y = z;
-                array[c+1] = z;
+                array[c+1] = array[c] + array[c+1];
                 c += 2;
             }
-        }
-        System.out.print("\nNew Array: ");
-        for (int i = 0; i < array.length; i++) {
-            System.out.print(array[i] + " ");
         }
     }
 }
